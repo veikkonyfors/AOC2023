d37bac7de1268dc3844a363425a573a0cae8238c
Veikko Nyfors <veikkonyfors@gmail.com>
2023-11-27 16:17:39 +0200

    Initial commit

---
+++ day01/src/main/java/fi/viware/AOC2023/day01/Day01.kt
@@ -0,0 +1,8 @@
+package fi.viware.AOC2023.day01
+
+class Day01 {
+
+    fun hello() {
+        println("Advent Of Code 2023 Day 1")
+    }
+}
---
+++ day01/src/test/java/fi/viware/AOC2023/day01/Day01Test.kt
@@ -0,0 +1,24 @@
+package fi.viware.AOC2023.day01
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class Day01Test {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun hello(){
+        val day01 = Day01()
+        day01.hello()
+    }
+}
