ea6a704696946e895ff04089319590b2428178c4
Veikko Nyfors <veikkonyfors@gmail.com>
2023-12-01 14:57:45 +0200

    Remove day01 kotlin module

--- day01/src/main/java/fi/viware/AOC2023/day01/Day01.kt
+++
@@ -1,8 +0,0 @@
-package fi.viware.AOC2023.day01
-
-class Day01 {
-
-    fun hello() {
-        println("Advent Of Code 2023 Day 1")
-    }
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,4 +14,3 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "AOC2023"
-include(":day01")
